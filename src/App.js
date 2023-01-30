7c406418c1aa0a56cd288186ed33949b6d5d6ba5
Kashaf <kashafmirza771@gmail.com>
2023-01-30 00:31:13 -0800

    Apply React dom

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-// import About from './components/About';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route
-// } from "react-router-dom";
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 
@@ -47,21 +47,21 @@ function App() {
 
   return (
     <>
-      {/* <Router> */}
+      <Router>
      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
      {/* state var alert is pass to alert */}
      <Alert alert={alert}/> 
       <div className="container my-3">
-      {/* <Switch> */}
-          {/* <Route exact path="/about"> */}
-            {/* <About  mode={mode} /> */}
-          {/* </Route>  */}
-          {/* <Route exact path="/"> */}
+      <Switch> 
+           <Route exact path="/about"> 
+             <About  mode={mode} /> 
+           </Route>  
+           <Route exact path="/">
           <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter,Character Counter,Remove extra spaces" mode={mode} />
-          {/* </Route> */}
-       {/* </Switch>     */}
+          </Route> 
+        </Switch>    
      </div> 
-     {/* </Router> */}
+     </Router>
     </>
   );
 }
